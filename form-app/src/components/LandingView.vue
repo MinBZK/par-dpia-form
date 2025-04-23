660c2753b7c7cfc0c171b69ff4568232a7b66998
Ruben Rouwhof <ruben.rouwhof@rijksoverheid.nl>
2025-04-23 16:25:10 +0200

    update text

--- form-app/src/components/LandingView.vue
+++ form-app/src/components/LandingView.vue
@@ -15,7 +15,7 @@ const props = defineProps<{
       <h1 class="utrecht-heading-1">Ondersteuning bij DPIA’s</h1>
       <p class="rvo-text--lg">
         Bij verwerkingen van persoonsgegevens is het belangrijk om vroegtijdig inzicht te krijgen in mogelijke privacyrisico’s. Deze tools helpen je daarbij. Ze zijn gebaseerd op het <a href="https://modellen.jenvgegevens.nl/dpia/#IntroPre-scanDPIA">Rijksmodel DPIA Rijksdienst</a> en sluiten aan op rijksbrede kaders, inclusief AI en kinderrechten.
-        Gebruik de Pre-scan voor een eerste inschatting. Is een DPIA nodig? Dan kun je direct verder met het rapportagemodel.
+        Gebruik de pre-scan om te bepalen of een DPIA, IAMA, KIA en/of DTIA moet worden uitgevoerd. Is een DPIA nodig? Dan kun je direct verder met het rapportagemodel.
       </p>
 
       <div class="rvo-layout-grid-container">
@@ -23,7 +23,7 @@ const props = defineProps<{
           <div class="rvo-card rvo-card--outline rvo-card--padding-md rvo-card__full-card-link">
             <div class="rvo-card__content">
               <h2 class="utrecht-heading-2">Pre-scan DPIA</h2>
-              <p>Met de pre-scan DPIA toets je of een volledige DPIA, IAMA of KIA nodig is. De tool bestaat uit een vragenlijst die automatisch een risicoscore berekent en geeft gericht vervolgadvies.</p>
+              <p>Met de pre-scan DPIA toets je of een DPIA, DTIA, IAMA of KIA nodig is. De tool bevat een vragenlijst die helpt bij het inschatten van risico’s en geeft op basis daarvan advies over het uitvoeren van een DPIA. Daarnaast geeft de tool ook advies over de noodzaak van een IAMA, KIA of DTIA.</p>
               <button class="utrecht-button utrecht-button--primary-action" @click="navigation.goToPreScanDPIA">
                 Start pre-scan DPIA
               </button>
@@ -32,7 +32,7 @@ const props = defineProps<{
           <div class="rvo-card rvo-card--outline rvo-card--padding-md rvo-card__full-card-link">
             <div class="rvo-card__content">
               <h2 class="utrecht-heading-2">DPIA Rapportagemodel</h2>
-              <p>Deze tool begeleidt je stap voor stap bij het uitvoeren van een volledige DPIA. De rapportage voldoet aan de eisen uit de AVG en het model DPIA Rijksdienst, en is geschikt voor verwerking in het verwerkingsregister.</p>
+              <p>Deze tool begeleidt je stap voor stap bij het uitvoeren van een DPIA. De rapportage voldoet aan de eisen uit de AVG en het model DPIA Rijksdienst, en is geschikt voor verwerking in het verwerkingsregister.</p>
               <button class="utrecht-button utrecht-button--primary-action" @click="navigation.goToDPIA">
                 Start DPIA Rapportagemodel
               </button>
