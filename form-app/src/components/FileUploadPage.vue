f9d128d18eb1d84a89d5f0a90f3f8288025f91d9
Ruben Rouwhof <ruben.rouwhof@rijksoverheid.nl>
2025-05-22 10:06:18 +0200

    Changes to text and layout (#193)

--- form-app/src/components/FileUploadPage.vue
+++ form-app/src/components/FileUploadPage.vue
@@ -14,6 +14,22 @@ const fileUploadError = ref<string | null>(null)
 const isProcessing = ref(false)
 const taskStore = useTaskStore()
 
+const introText = computed(() => {
+  if (taskStore.activeNamespace === 'dpia') {
+    return "Deze tool begeleidt je stap voor stap bij het uitvoeren van een DPIA. De rapportage voldoet aan de eisen uit de AVG en het model DPIA Rijksdienst, en is geschikt voor verwerking in het verwerkingsregister.";
+  } else {
+    return 'Met de pre-scan toets je of een DPIA, DTIA, IAMA of KIA nodig is. De tool bevat een vragenlijst die helpt bij het inschatten van risicos en geeft op basis daarvan advies over het uitvoeren van een assessment.';
+  }
+})
+
+const uploadText = computed(() => {
+  if (taskStore.activeNamespace === 'dpia') {
+    return 'Heb je al eerder een pre-scan of DPIA ingevuld voor deze gegevensverwerking?.';
+  } else {
+    return 'Heb je al eerder een pre-scan ingevuld voor deze gegevensverwerking?';
+  }
+})
+
 const handleFileSelect = (event: Event) => {
   const target = event.target as HTMLInputElement
   console.log(target)
@@ -58,29 +74,26 @@ const startDpia = async () => {
 }
 
 const formTypeLabel = computed(() => {
-  return taskStore.activeNamespace === 'dpia' ? 'DPIA' : 'Pre-scan DPIA'
+  return taskStore.activeNamespace === 'dpia' ? 'DPIA' : 'pre-scan'
 })
 </script>
 
 <template>
-  <h1 class="utrecht-heading-1">Welkom bij de {{ formTypeLabel }} tool</h1>
+  <h1 class="utrecht-heading-1">Start de {{ formTypeLabel }}</h1>
 
   <div class="utrecht-form-fieldset rvo-form-fieldset">
     <fieldset class="utrecht-form-fieldset__fieldset utrecht-form-fieldset--html-fieldset">
-      <legend class="utrecht-form-fieldset__legend utrecht-form-fieldset__legend--html-legend">
-        Heeft u al een eerdere versie van de DPIA of pre-scan beschikbaar?
-      </legend>
-
-      <div class="utrecht-form-field-description" id="file-upload-helper">
-        Upload hieronder een eerder opgeslagen bestand om deze informatie te laden.
+      <div class="utrecht-form-field-description" id="file-upload-helper" v-html="introText"></div>
+      <div class="rvo-layout-margin-vertical--lg">
       </div>
-
+    </fieldset>
+    <fieldset class="utrecht-form-fieldset__fieldset utrecht-form-fieldset--html-fieldset">
       <div class="rvo-layout-margin-vertical--lg">
         <div role="group" aria-labelledby="file-upload-label"
           class="utrecht-form-field utrecht-form-field--text rvo-form-field">
           <div class="rvo-form-field__label">
             <label class="rvo-label" id="file-upload-label" for="file-upload-field">
-              Bestand
+              {{ uploadText }}
             </label>
           </div>
           <input id="file-upload-field" type="file" class="rvo-file-input" accept=".json" @change="handleFileSelect" />
